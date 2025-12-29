b71c51086db659b65270af68f6237eefef3d5dd8
LePhuThanh <lpthanh710@gmail.com>
2025-12-30 02:07:42 +0700

    Handle Input File from disk by two way (Common & Lambda Expression)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileFilter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("================>Hello");
+        //Old way
+//        FileFilter fileFilter = new FileFilter() {
+//            @Override
+//            public boolean accept(File pathname) {
+//                return pathname.getName().endsWith(".java"); // Check java file
+//            }
+//        };
+        //Lambda Expression (Java 8)
+        FileFilter fileFilter = (File pathname) -> pathname.getName().endsWith(".java");
+
+        File dir = new File("d:/Visual_Studio_Code/Udemy/Java8Masterclass/file"); // D:\Visual_Studio_Code\Udemy\Java8Masterclass\file
+        File[] javaFiles = dir.listFiles(fileFilter);
+        for(File file : javaFiles){
+            System.out.println("File name: " + file.getName());
+        }
     }
 }
