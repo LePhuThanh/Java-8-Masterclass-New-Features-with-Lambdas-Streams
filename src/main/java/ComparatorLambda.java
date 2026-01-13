a100ee50cf6d0c8eeaf4ebbe1b231a0c965bc00d
LePhuThanh <lpthanh710@gmail.com>
2026-01-13 22:50:48 +0700

    Lambda Expression with Comparator method

---
+++ src/main/java/ComparatorLambda.java
@@ -0,0 +1,27 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorLambda {
+    public static void main(String[] args) {
+//        Comparator<String> comp = new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return Integer.compare(o1.length(), o2.length());
+//            }
+//        };
+
+        //Lambda Expression
+        Comparator<String> comp = (o1, o2) -> Integer.compare(o1.length(),o2.length());
+
+        List<String> list = Arrays.asList("***","**","*", "****");
+        Collections.sort(list, comp);
+
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+}
