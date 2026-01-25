a2ca13c03519ae3b4d50771a2562b2df0482395c
LePhuThanh <lpthanh710@gmail.com>
2026-01-25 12:54:59 +0700

    map & flatMap in stream

---
+++ src/main/java/stream/FlapMapExample.java
@@ -0,0 +1,41 @@
+package main.java.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class FlapMapExample {
+    public static void main(String[] args){
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        List<Integer> list2 = Arrays.asList(2, 4, 6);
+        List<Integer> list3 = Arrays.asList(3, 5, 7);
+
+        List<List<Integer>> list =  Arrays.asList(list1, list2, list3);
+        System.out.println(list);
+
+        //Way1
+//        list.stream()
+//                .map(List::size)
+//                .forEach(System.out::println);
+
+
+        //Way2
+//        Function<List<?>, Integer> size = List::size;
+//        list.stream()
+//                .map(size)
+//                .forEach(System.out::println);
+
+
+
+        Function<List<Integer> , Stream<Integer>> flatmapper = l -> l.stream();
+        //map
+        list.stream()
+                .map(flatmapper) //Stream<Stream<Integer>>
+                .forEach(System.out::println);
+        //flatMap
+        list.stream()
+                .flatMap(flatmapper) //Stream<Stream<Integer>> -> Stream<Integer>
+                .forEach(System.out::println);
+    }
+}
